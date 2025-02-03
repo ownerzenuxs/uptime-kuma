3d49183c0bb776ed1e6b7d1d2379937bd69ca10b
ownerzenuxs <iwantvpspls@gmail.com>
2025-02-03 12:15:59 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,7 @@
-echo Hello
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc
+nvm install 22.8.0
+nvm use 22.8.0
+git clone https://github.com/louislam/uptime-kuma.git
+cd uptime-kuma
+npm run setup
